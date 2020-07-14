46d0e285d83b6f48a3edcf10dd87ea6521664983
Zeb Ford-Reitz <zeb.fordreitz@gmail.com>
2020-07-14 23:42:19 +0200

    Adds goblins to starting zones

--- src/Board.tsx
+++ src/Board.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import { BoardProps } from 'boardgame.io';
 import './App.css';
+import { GameState } from './GameState';
 
-export class DragonPooBoard extends React.Component<BoardProps> {
+export class DragonPooBoard extends React.Component<BoardProps<GameState>> {
   render() {
     const cellStyle = {
       border: '1px solid #555',
@@ -18,24 +19,26 @@ export class DragonPooBoard extends React.Component<BoardProps> {
         const id = 5 * i + j;
         cells.push(
           <td style={cellStyle} key={id}>
-            {this.props.G.cells[i][j]}
+            {this.props.G.cells[i][j].join()}
           </td>
         );
       }
       tbody.push(<tr key={i}>{cells}</tr>);
     }
 
+    const piecesOnBoard = this.props.G.cells.flat(Infinity);
+
     return (
       <div className="grid">
-        <div className="start-zone-1"></div>        
-        <div className="start-zone-2"></div>        
+        <div className="start-zone-1">{piecesOnBoard.includes('0') ? '' : '0'}</div>
+        <div className="start-zone-2">{piecesOnBoard.includes('1') ? '' : '1'}</div>
         <div className="center">
           <table id="board">
             <tbody>{tbody}</tbody>
           </table>
         </div>
-        <div className="start-zone-3"></div>        
-        <div className="start-zone-4"></div>        
+        <div className="start-zone-3">{piecesOnBoard.includes('2') ? '' : '2'}</div>
+        <div className="start-zone-4">{piecesOnBoard.includes('3') ? '' : '3'}</div>
       </div>
     );
   }
